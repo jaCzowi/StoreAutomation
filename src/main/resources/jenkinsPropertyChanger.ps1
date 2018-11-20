7942bbf4f28c744130d996aff6336c2331d7ba6d
Wiktor Religo <wreligo@pl.sii.eu>
2018-11-20 19:01:18 +0100

    making corrections in powershell script'

--- src/main/resources/jenkinsPropertyChanger.ps1
+++ src/main/resources/jenkinsPropertyChanger.ps1
@@ -1,7 +1,8 @@
 [String]$Browser = "browser"
+[String]$nameBrowser= $name
 
 gc  browser.properties | Foreach-Object {
-	$_ -replace "$Browser.+", "$Browser=$env:Browser" `
+	$_ -replace "$Browser.+", "$Browser=$env:Browser"`
 	} | Set-Content tempBrowser
 	rm browser.properties
-	rename-item  browser.properties
+	rename-item tempBrowser browser.properties
